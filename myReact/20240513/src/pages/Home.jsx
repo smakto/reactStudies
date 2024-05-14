03f10042c3a7c1cacabe46a070deee6d3b499eeb
smakto <raizyssarunas@gmail.com>
2024-05-14 22:00:28 +0300

    Progress on restaurant app.

--- myReact/20240513/src/pages/Home.jsx
+++ myReact/20240513/src/pages/Home.jsx
@@ -1,7 +1,22 @@
+import { useState, useEffect } from "react";
+
 export function Home() {
+  const [currentOrders, setCurrentOrders] = useState([]);
+
+  useEffect(() => {
+    let storedData = JSON.parse(localStorage.getItem("orders"));
+    storedData ? setCurrentOrders(storedData) : setCurrentOrders([]);
+  }, []);
+
   return (
     <main>
-      <div>Home</div>
+      {currentOrders.map((items) => {
+        return (
+          <div
+            key={items.id}
+          >{`Order ID: ${items.id}. Headcount: ${items.peopleCount}. Total order sum ${items.checkSum}`}</div>
+        );
+      })}
     </main>
   );
 }
